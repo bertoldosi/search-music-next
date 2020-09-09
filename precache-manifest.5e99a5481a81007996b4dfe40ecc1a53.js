47467388f2998baceef5ca59e1dbcea87853e7f6
bertoldo <matheus.bertoldosi@gmail.com>
2020-09-09 17:10:40 -0500

    Updates

---
+++ precache-manifest.5e99a5481a81007996b4dfe40ecc1a53.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "1e7270454f72cebdaef2c841300d4b58",
+    "url": "/search-music-last-fm/index.html"
+  },
+  {
+    "revision": "0bddb4e32231efae60c3",
+    "url": "/search-music-last-fm/static/css/main.2936a7a3.chunk.css"
+  },
+  {
+    "revision": "abca239b1462c4356b05",
+    "url": "/search-music-last-fm/static/js/2.1d3b9035.chunk.js"
+  },
+  {
+    "revision": "c64c486544348f10a6d6c716950bc223",
+    "url": "/search-music-last-fm/static/js/2.1d3b9035.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "0bddb4e32231efae60c3",
+    "url": "/search-music-last-fm/static/js/main.00849b9f.chunk.js"
+  },
+  {
+    "revision": "632b4e39c829866c9f37",
+    "url": "/search-music-last-fm/static/js/runtime-main.6cbb6a5e.js"
+  }
+]);
